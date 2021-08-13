9ddf51938a93bcb70e1a71c628088d5bcca40534
Crowdbotics <team@crowdbotics.com>
2021-08-13 18:30:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "twilight_sound_29789Identifier",
+  url: "https://twilight_sound_29789Identifier.botics.co",
+  api: "https://twilight_sound_29789Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
